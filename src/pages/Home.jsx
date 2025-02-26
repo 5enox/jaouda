10b01594861098cd4e8ea4321c84b7ffba24b3a2
b-lkbih <alqrsanalmttwr@gmail.com>
2025-02-26 20:22:44 +0000

    clrearing home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import Navbar from '../components/navigation/navbar.jsx'
-import Hero from '../components/hero section/hero.jsx'
-import Botton from '../components/botton/botton.jsx'
 import TextSection from '../components/textSection/textsection.jsx'
 
 
