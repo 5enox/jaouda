6deabf39380ca2dc121c41f4fea765114e9fa39d
b-lkbih <alqrsanalmttwr@gmail.com>
2025-07-23 12:26:42 +0100

    changing a the hero img

--- src/components/hero section/hero.jsx
+++ src/components/hero section/hero.jsx
@@ -1,5 +1,5 @@
 import "./hero.css";
-import "../../assets/pictures/real pic/WhatsApp Image 2025-02-13 at 15.36.29_c6e81e12.jpg";
+import MainPic from "../../assets/pictures/real pic/WhatsApp Image 2025-02-13 at 15.36.29_c6e81e12.jpg";
 import { useTranslation } from "react-i18next";
 
 export default function hero() {
@@ -9,7 +9,7 @@ export default function hero() {
     <header>
       <div className="gradientbackgraound"></div>
       <div className="hero-text" >
-        <img className="heroPicture" src="src/assets/pictures/real pic/WhatsApp Image 2025-02-13 at 15.36.29_c6e81e12.jpg" alt="apartement" />
+        <img className="heroPicture" src={MainPic} alt="apartement" />
         <h2 className="hero-text-title">
           {t('hero.title')}
         </h2>
